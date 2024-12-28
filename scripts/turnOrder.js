1eff3655e9cbf99b485f86e3d530bd2da53a862b
Niqu3d <niqu3d@niqu3d.io>
2024-12-28 15:11:59 -0500

    testing
    testing

--- scripts/turnOrder.js
+++ scripts/turnOrder.js
@@ -4,8 +4,8 @@ export class TurnOrder {
   static getTurnOrder() {
     const actors = game.actors.contents;
     return actors.sort((a, b) => {
-      const aInit = a.getRollData().initiative.value;
-      const bInit = b.getRollData().initiative.value;
+      const aInit = a.getRollData().initiative.value || 0; // Handle actors without initiative
+      const bInit = b.getRollData().initiative.value || 0;
       return bInit - aInit;
     });
   }
